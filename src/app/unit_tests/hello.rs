c1613c4ef0c9e2ce5903747724185accf51a6da4
Nathan Flint <nlflint@gmail.com>
2020-04-01 15:32:34 -0700

    WIP: Creating SecondClient

--- src/app/unit_tests/hello.rs
+++ src/app/unit_tests/hello.rs
@@ -1,4 +1,27 @@
 use super::*;
+use net2::TcpBuilder;
+use ws::{WebSocket, Sender, Factory, Handler};
+use url::Url;
+
+struct SecondClient {
+
+}
+
+impl Factory for SecondClient {
+    type Handler = impl Handler;
+
+    fn connection_made(&mut self, out: Sender) -> Self::Handler {
+        out.send(bincode::serialize(&ChatMessage::Hello).unwrap()).unwrap();
+
+        // move |msg: ws::Message| {
+        //     // match msg {
+        //     //     ws::Message::Binary(data) => {
+        //     //     },
+        //     //     _ => panic!("We expected a ws::Message::Binary")
+        //     // };
+        // }
+    }
+}
 
 #[test]
 fn hello__new_server_responds_with_empty_ip_list() {
@@ -13,13 +36,24 @@ fn hello__new_server_responds_with_empty_ip_list() {
 }
 
 #[test]
-fn hello__() {
+fn hello__second_client_receives_non_empty_ip_list() {
     // Given
     let _app = App::start().unwrap();
+
+    // established connecgtion
+    let socket = WebSocket::new(SecondClient {})
+        .unwrap()
+        .bind("127.0.0.2:4445")
+        .unwrap()
+        .connect(Url::Parse("ws://127.0.0.1:4444"))
+        .unwrap()
+        .run()
+        .unwrap();
+
 
     // when
     let response = send_message(&ChatMessage::Hello);
 
     // Then
-    assert_eq!(response, ChatMessage::IpList(Vec::<Ipv4Addr>::new()));
+    assert_eq!(response, ChatMessage::IpList(vec![Ipv4Addr::new(127,0,0,2)]));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![feature(type_alias_impl_trait)]
 mod app;
 mod error;
 use error::Error;
