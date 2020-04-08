3a2e1d089673568377f7a9ec3de8da448689ddf5
Nathan Flint <nate.flint@tpfs.io>
2020-04-08 15:30:51 -0700

    WIP: Created channel which is being dropped for some reason

--- src/app/unit_tests/hello.rs
+++ src/app/unit_tests/hello.rs
@@ -2,24 +2,30 @@ use super::*;
 use net2::TcpBuilder;
 use ws::{WebSocket, Sender, Factory, Handler};
 use url::Url;
+use std::thread;
+use std::sync::mpsc::{channel, Sender as ChannelSender};
+use std::time::{Duration, Instant};
 
-struct SecondClient {
-
+struct FirstClient {
+    sender: Option<ChannelSender<()>>,
 }
 
-impl Factory for SecondClient {
-    type Handler = impl Handler;
+impl Factory for FirstClient {
+    type Handler = impl Fn(ws::Message) -> Result<(), ws::Error>;
+    // type Handler = impl Handler;
 
     fn connection_made(&mut self, out: Sender) -> Self::Handler {
         out.send(bincode::serialize(&ChatMessage::Hello).unwrap()).unwrap();
 
-        // move |msg: ws::Message| {
-        //     // match msg {
-        //     //     ws::Message::Binary(data) => {
-        //     //     },
-        //     //     _ => panic!("We expected a ws::Message::Binary")
-        //     // };
-        // }
+
+
+        let channel_sender = self.sender.take().unwrap();
+
+        move |msg: ws::Message| {
+            panic!("inside msg handler");
+            //channel_sender.send(());
+            Ok(())
+        }
     }
 }
 
@@ -40,16 +46,27 @@ fn hello__second_client_receives_non_empty_ip_list() {
     // Given
     let _app = App::start().unwrap();
 
-    // established connecgtion
-    let socket = WebSocket::new(SecondClient {})
-        .unwrap()
-        .bind("127.0.0.2:4445")
-        .unwrap()
-        .connect(Url::Parse("ws://127.0.0.1:4444"))
-        .unwrap()
-        .run()
-        .unwrap();
+    let (sender, receiver) = channel();
 
+    // established connection
+    let handler = thread::spawn(|| {
+        let mut socket = WebSocket::new(FirstClient {sender: Some(sender)})
+            .unwrap()
+            .bind("127.0.0.2:4445")
+            .unwrap();
+
+        socket
+            .connect(Url::parse("ws://127.0.0.1:4444").unwrap())
+            .unwrap();
+
+        socket
+            .run()
+            .unwrap();
+    });
+
+    println!("Before! {:?}", Instant::now());
+    dbg!(receiver.recv_timeout(Duration::from_secs(10)));
+    println!("After! {:?}", Instant::now());
 
     // when
     let response = send_message(&ChatMessage::Hello);
